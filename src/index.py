64b7658b66d0c22fde7c21543690a2395cc179c0
xukecheng <cnxukecheng@gmail.com>
2022-06-14 14:41:20 +0800

    补充逻辑

--- src/index.py
+++ src/index.py
@@ -5,17 +5,20 @@ from qcloud_cos import CosS3Client
 import sys
 import logging
 import requests
+import time
+import os
 
 # 正常情况日志级别使用INFO，需要定位时可以修改为DEBUG，此时SDK会打印和服务端的通信信息
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
-# 1. 设置用户属性, 包括 secret_id, secret_key, region等。Appid 已在CosConfig中移除，请在参数 Bucket 中带上 Appid。Bucket 由 BucketName-Appid 组成
-secret_id = "SecretId"  # 替换为用户的 SecretId，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
-secret_key = "SecretKey"  # 替换为用户的 SecretKey，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
-region = "ap-beijing"  # 替换为用户的 region，已创建桶归属的region可以在控制台查看，https://console.cloud.tencent.com/cos5/bucket
-# COS支持的所有region列表参见https://cloud.tencent.com/document/product/436/6224
-token = None  # 如果使用永久密钥不需要填入token，如果使用临时密钥需要填入，临时密钥生成和使用指引参见https://cloud.tencent.com/document/product/436/14048
-scheme = "https"  # 指定使用 http/https 协议来访问 COS，默认为 https，可不填
+secret_id = os.getenv("SecretId")
+secret_key = os.getenv("SecretKey")
+region = os.getenv("region")
+token = None
+scheme = "https"
+bucket = os.getenv("Bucket")
+cdn_url = os.getenv("CDNURL")
+
 
 config = CosConfig(
     Region=region, SecretId=secret_id, SecretKey=secret_key, Token=token, Scheme=scheme
@@ -24,11 +27,17 @@ client = CosS3Client(config)
 
 
 def main_handler(event, context):
+    logger.info("start main handler")
+
+    path_parameters = event["pathParameters"]
+    extension_name = path_parameters["extension_name"]
+    filename = "%s%s" % (int(round(time.time() * 1000)), f".{extension_name}")
+
     # 生成上传URL，未限制请求头部和请求参数
     url = client.get_presigned_url(
         Method="PUT",
-        Bucket="examplebucket-1250000000",
-        Key="exampleobject",
+        Bucket=bucket,
+        Key=f"{extension_name}/{filename}",
         Expired=120,  # 120秒后过期，过期时间请根据自身场景定义
     )
     print(url)
@@ -36,7 +45,10 @@ def main_handler(event, context):
         "isBase64Encoded": false,
         "statusCode": 200,
         "headers": {"Content-Type": "application/json"},
-        "body": {"read_url": "", "upload_url": ""},
+        "body": {
+            "read_url": f"{cdn_url}/{extension_name}/{filename}",
+            "upload_url": url,
+        },
     }
 
 
