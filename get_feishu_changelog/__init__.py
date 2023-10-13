4a633755e1d73c12e9cec9d4fcffdd1ae4261246
xukecheng <cnxukecheng@gmail.com>
2023-10-13 18:28:56 +0800

    fix(__init__.py): add support for BROWSERLESS_TOKEN environment variable feat(pyproject.toml): add pyproject.toml file with project dependencies

--- get_feishu_changelog/__init__.py
+++ get_feishu_changelog/__init__.py
@@ -34,9 +34,10 @@ def parse_html() -> List[Dict[str, str]]:
     - "description": the text content of the paragraph.
     """
     browserless_url = os.environ.get("BROWSERLESS_URL")
+    browserless_token = os.environ.get("BROWSERLESS_TOKEN")
 
     x = requests.post(
-        browserless_url,
+        f"{browserless_url}/content?token={browserless_token}",
         json={
             "url": "https://www.feishu.cn/hc/zh-CN/articles/360049067483",
             "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36 Edg/81.0.416.64",
